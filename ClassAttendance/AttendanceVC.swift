4a03ce13cc5c1251acaa1863cda4d1bd2980cfea
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-05 17:56:30 -0300

    Setting Up Proyect

---
+++ ClassAttendance/AttendanceVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ClassAttendance
+//
+//  Created by Mago Nicolas Palacios on 9/5/16.
+//  Copyright © 2016 Mago Nicolas Palacios. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
